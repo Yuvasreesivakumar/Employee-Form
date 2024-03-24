e507bb644a598a726691f5186f2404fa17dab2a6
Yuvasreesivakumar <yuvasreesivakumar04@gmail.com>
2024-03-24 09:51:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component} from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formdb';
+  title = 'form';
+  userArray: Array<any> = [];
+  user: any = {};
+  age: number | null = null;
+  Dept=['FullStack','Cybersecurity','Data Analyst','Software Development','Cloud Computing'];
+  employees: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  onClick() {
+    this.calculateAge();
+    this.userArray.push({
+      name: this.user.name,
+      id: this.user.id,
+      dept: this.user.dept,
+      salary: this.user.salary,
+      experience: this.user.experience,
+      dob:this.user.dob,
+      age:this.age,
+    });
+
+
+
+  }
+
+  calculateAge() {
+    if (this.user.dob) {
+      const today = new Date();
+      const birthDate = new Date(this.user.dob);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+
+      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      this.age = age;
+    } else {
+      this.age = null;
+    }
+  }
+
+  onDelete(index: number) {
+    if (index >= 0 && index < this.userArray.length) {
+      this.userArray.splice(index, 1);
+    }
+  }
+
+  onSubmit(eform: any) {
+    console.log('Form submitted:', eform.value);
+
+  }
 }
